1b0694c26506e4977b5dd157dfd7998540e4cc4d
Ascool <81183594+Ascool7776@users.noreply.github.com>
2021-03-30 09:49:00 +0530

    Add files via upload

--- Virtual_Assistant.py
+++ Virtual_Assistant.py
@@ -7,35 +7,39 @@ import os                          #To open files
 import time                        #To calculate time
 import subprocess                  #To open files
 from tkinter import *              #For the graphics
-import pyjokes
-from playsound import playsound
-
-def delete1():
-    screen.destroy()
+import pyjokes                     #For some really bad jokes
+from playsound import playsound    #To playsound
+import keyboard                    #To get keyboard
   
-file1 = open("Assistant_name", "r")
-name_assistant = file1.read()
+name_file = open("Assistant_name", "r")
+name_assistant = name_file.read()
+
 engine = pyttsx3.init('sapi5')  
 voices = engine.getProperty('voices')  
 engine.setProperty('voice', voices[1].id)
-
-
+    
 def speak(text):
     engine.say(text)
     print(name_assistant + " : "  +  text)
-    engine.runAndWait()
+    engine.runAndWait() 
 
 
 def wishMe():
-    hour=datetime.datetime.now().hour
-    if hour>=0 and hour<12:
-        speak("Hello,Good Morning")
-   
-    elif hour>=12 and hour<18:
-        speak("Hello,Good Afternoon")
+
+
+  hour=datetime.datetime.now().hour
+
+  if hour >= 0 and hour < 12:
+
+      speak("Hello,Good Morning")
  
-    else:
-        speak("Hello,Good Evening")
+  elif hour >= 12 and hour < 18:
+
+      speak("Hello,Good Afternoon")
+
+  else:
+
+      speak("Hello,Good Evening")
 
 
 def get_audio(): 
@@ -57,11 +61,9 @@ def get_audio():
         return text
 
 
-    except Exception as e:
+    except:
 
-        
         return "None"
-        return statement
 
 
 def note(text):
@@ -76,12 +78,10 @@ def note(text):
 
 def date():
     now = datetime.datetime.now()
-    my_date = datetime.datetime.today()
     month_name = now.month
     day_name = now.day
     month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     ordinalnames = [ '1st', '2nd', '3rd', ' 4th', '5th', '6th', '7th', '8th', '9th', '10th', '11th', '12th', '13th', '14th', '15th', '16th', '17th', '18th', '19th', '20th', '21st', '22nd', '23rd','24rd', '25th', '26th', '27th', '28th', '29th', '30th', '31st'] 
-    
 
     speak("Today is "+ month_names[month_name-1] +" " + ordinalnames[day_name-1] + '.')
 
@@ -94,23 +94,36 @@ def Process_audio():
     if __name__=='__main__':
         while run==1:
 
+            app_string = ["open word", "open powerpoint", "open excel", "open zoom","open notepad",  "open chrome"]
+            app_link = [r'\Microsoft Office Word 2007.lnk',r'\Microsoft Office PowerPoint 2007.lnk', r'\Microsoft Office Excel 2007.lnk', r'\Zoom.lnk', r'\Notepad.lnk', r'\Google Chrome.lnk' ]
+            app_dest = r'C:\Users\shriraksha\AppData\Roaming\Microsoft\Windows\Start Menu\Programs'
+
             statement = get_audio().lower()
             results = ''
             run +=1
 
+            if "hello" in statement or "hi" in statement:
+
+              wishMe()               
+
+
             if "good bye" in statement or "ok bye" in statement or "stop" in statement:
-                speak('your personal assistant ' + name_assistant +' is shutting down, Good bye')
-                delete1()
+                speak('Your personal assistant ' + name_assistant +' is shutting down, Good bye')
+                screen.destroy()
                 break
 
             if 'wikipedia' in statement:
+              try:
 
 
-              speak('Searching Wikipedia...')
-              statement = statement.replace("wikipedia", "")
-              results = wikipedia.summary(statement, sentences = 3)
-              speak("According to Wikipedia")
-              speak(results)
+                speak('Searching Wikipedia...')
+                statement = statement.replace("wikipedia", "")
+                results = wikipedia.summary(statement, sentences = 3)
+                speak("According to Wikipedia")
+                wikipedia_screen(results)
+              except:
+                speak("Error")
+
 
             if 'joke' in statement:
               speak(pyjokes.get_joke())    
@@ -137,123 +150,156 @@ def Process_audio():
                     speak("Netflix open now")
 
 
-            if 'open primevideo' in statement:
+            if 'open prime video' in statement:
                     webbrowser.open_new_tab("primevideo.com") 
                     speak("Amazon Prime Video open now")
                     time.sleep(5)
 
-            if 'open word' in statement:
-                os.startfile(r'C:\Users\shriraksha\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows 7\Microsoft Office Word 2007')
-                speak("Microsoft office word is opening now")
+            if app_string[0] in statement:
+                os.startfile(app_dest + app_link[0])
 
-            if 'open powerpoint' in statement:
-                os.startfile(r'C:\Users\shriraksha\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows 7\Microsoft Office PowerPoint 2007')
+                speak("Microsoft office Word is opening now")
+
+            if app_string[1] in statement:
+                os.startfile(app_dest + app_link[1])
                 speak("Microsoft office PowerPoint is opening now")
 
-            if 'open excel' in statement:
-                os.startfile(r'C:\Users\shriraksha\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows 7\Microsoft Office Excel 2007')
+            if app_string[2] in statement:
+                os.startfile(app_dest + app_link[2])
                 speak("Microsoft office Excel is opening now")
         
-            if 'open notepad' in statement:
-                os.startfile(r'C:\Users\shriraksha\AppData\Roaming\Microsoft\Windows\Start Mename\Programs\Accessories\Notepad.lnk')
-                speak("NotePad is opening now")
+            if app_string[3] in statement:
+
+                os.startfile(app_dest + app_link[3])
+                speak("Zoom is opening now")
 
 
-            if 'open chrome' in statement:
-                os.startfile(r'C:\Users\shriraksha\AppData\Roaming\Microsoft\Windows\Start Mename\Programs\Google Chrome.lnk')
-                speak("Google chrome is opening now")
+            if app_string[4] in statement:
+                os.startfile(app_dest + app_link[4])
+                speak("Notepad is opening now")
         
-            if 'open zoom' in statement:
-                os.startfile(r'C:\Users\shriraksha\AppData\Roaming\Microsoft\Windows\Start Mename\Programs\Zoom.lnk')
+            if app_string[5] in statement:
+                os.startfile(app_dest + app_link[5])
                 speak("Google chrome is opening now")
                        
 
-            elif 'news' in statement:
+            if 'news' in statement:
                 news = webbrowser.open_new_tab("https://timesofindia.indiatimes.com/city/mangalore")
                 speak('Here are some headlines from the Times of India, Happy reading')
                 time.sleep(6)
 
-            elif 'cricket' in statement:
+            if 'cricket' in statement:
                 news = webbrowser.open_new_tab("cricbuzz.com")
                 speak('This is live news from cricbuzz')
                 time.sleep(6)
 
-            elif 'corona' in statement:
+            if 'corona' in statement:
                 news = webbrowser.open_new_tab("https://www.worldometers.info/coronavirus/")
-                speak('Here are the latest covid-19 namembers')
+                speak('Here are the latest covid-19 numbers')
                 time.sleep(6)
 
-            elif 'time' in statement:
+            if 'time' in statement:
                 strTime=datetime.datetime.now().strftime("%H:%M:%S")
                 speak(f"the time is {strTime}")
+
             if 'date' in statement:
                 date()
-            elif 'who are you' in statement or 'what can you do' in statement:
+
+            if 'who are you' in statement or 'what can you do' in statement:
                     speak('I am '+name_assistant+' your personal assistant. I am programmed to minor tasks like opening youtube, google chrome, gmail and search wikipedia etcetra') 
 
 
-            elif "who made you" in statement or "who created you" in statement or "who discovered you" in statement:
+            if "who made you" in statement or "who created you" in statement or "who discovered you" in statement:
                 speak("I was built by Abhhi  Sannayya")
 
             
-            elif 'make a note' in statement:
+            if 'make a note' in statement:
                 statement = statement.replace("make a note", "")
                 note(statement)
 
 
-            elif 'note this' in statement:    
+            if 'note this' in statement:    
                 statement = statement.replace("note this", "")
-                note(statement)           
+                note(statement)         
 
             speak(results)
 
 
 def change_name():
 
-  global name_info
-
   name_info = name.get()
+
   file=open("Assistant_name", "w")
-  file.write(name_info+"\n")
+
+  file.write(name_info)
+
   file.close()
 
-  name_entry.delete(0, END)
+  settings_screen.destroy()
+
+  screen.destroy()
 
 
-def change():
+def change_name_window():
     
-      global screen1
-      screen1 = Toplevel(screen)
-      screen1.title("Settings")
-      screen1.geometry("300x250")
-      
+      global settings_screen
       global name
-      global name_entry
+
+
+      settings_screen = Toplevel(screen)
+      settings_screen.title("Settings")
+      settings_screen.geometry("300x300")
+      settings_screen.iconbitmap('app_icon.ico')
+
       
       name = StringVar()
 
-      Label(screen1, text = "Changes will take place if you ").pack()
-      Label(screen1, text = "close the main window and open it again ").pack()
-      Label(screen1, text = "").pack()  
-      Label(screen1, text = "Current name: "+name_assistant).pack()
-      Label(screen1, text = "Please enter your Virtual Assistant's name below").pack()    
-      Label(screen1, text = "").pack()
-      Label(screen1, text = "Name").pack()
-     
-      name_entry = Entry(screen1, textvariable =name)
-      name_entry.pack()
+      current_label = Label(settings_screen, text = "Current name: "+ name_assistant)
+      current_label.pack()
+
+      enter_label = Label(settings_screen, text = "Please enter your Virtual Assistant's name below") 
+      enter_label.pack(pady=10)   
       
 
-      Label(screen1, text = "").pack()
-      Button(screen1, text = "Ok", width = 10, height = 1, command = change_name).pack()
+      Name_label = Label(settings_screen, text = "Name")
+      Name_label.pack(pady=10)
+     
+      name_entry = Entry(settings_screen, textvariable = name)
+      name_entry.pack()
+
+
+      change_name_button = Button(settings_screen, text = "Ok", width = 10, height = 1, command = change_name)
+      change_name_button.pack(pady=10)
 
 
 def info():
-  screen2 = Toplevel(screen)
-  screen2.title("Info")
-  screen2.geometry("150x100")
-  Label(screen2,text = "Created by Abhhi Sannayya").pack()
-  Label(screen2, text = "For Makerspace").pack()
+
+  info_screen = Toplevel(screen)
+  info_screen.title("Info")
+  info_screen.iconbitmap('app_icon.ico')
+
+  creator_label = Label(info_screen,text = "Created by Abhhi Sannayya")
+  creator_label.pack()
+
+  Age_label = Label(info_screen, text= " at the age of 12")
+  Age_label.pack()
+
+  for_label = Label(info_screen, text = "For Makerspace")
+  for_label.pack()
+
+keyboard.add_hotkey("F4", Process_audio)
+
+
+def wikipedia_screen(text):
+
+
+  wikipedia_screen = Toplevel(screen)
+  wikipedia_screen.title(text)
+  wikipedia_screen.iconbitmap('Trial.ico')
+
+  message = Message(wikipedia_screen, text= text)
+  message.pack()
+
 
 
 def main_screen():
@@ -261,16 +307,24 @@ def main_screen():
       global screen
       screen = Tk()
       screen.title(name_assistant)
-      screen.geometry("150x350")
-      Label(text = name_assistant, bg = "grey", width = "300", height = "2", fg="white", font = ("Calibri", 15)).pack()
-      Label(text = "").pack()
-      photo1 = PhotoImage(file = "Microphone_on.png")
-      Button(image=photo1, command = Process_audio).pack()
-      Label(text = "").pack()
-      photo2 = PhotoImage(file = "settings.png")
-      Button(image=photo2, command =change).pack()
-      Label(text = "").pack()
-      Button(text ="Info", width = 20, height = 4, command = info).pack()
+      screen.geometry("100x250")
+      screen.iconbitmap('app_icon.ico')
+
+
+      name_label = Label(text = name_assistant,width = 300, bg = "black", fg="white", font = ("Calibri", 13))
+      name_label.pack()
+
+
+      microphone_photo = PhotoImage(file = "assistant_logo.png")
+      microphone_button = Button(image=microphone_photo, command = Process_audio)
+      microphone_button.pack(pady=10)
+
+      settings_photo = PhotoImage(file = "settings.png")
+      settings_button = Button(image=settings_photo, command = change_name_window)
+      settings_button.pack(pady=10)
+       
+      info_button = Button(text ="Info", command = info)
+      info_button.pack(pady=10)
 
       screen.mainloop()
 
